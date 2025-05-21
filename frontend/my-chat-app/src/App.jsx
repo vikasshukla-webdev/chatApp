591303008fbf87f5293d8a07a176b5e26ea58618
vikasshukla-webdev <shuklavikas.2807@gmail.com>
2025-05-21 13:56:53 +0530

    Update App.jsx

--- frontend/my-chat-app/src/App.jsx
+++ frontend/my-chat-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
-const socket = io('http://localhost:3001');
+const socket = io('https://chatapp-x8da.onrender.com');
 
 function App() {
   const [message, setMessage] = useState('');
